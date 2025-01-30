3d8f099d1f5af6bf91f25acf0e8ebf44a1b1eab4
Zasco <zasco_0@protonmail.com>
2025-01-30 10:50:39 -0500

    Move manifest to root folder

--- src/js/extension.js
+++ src/js/extension.js
@@ -1,5 +1,5 @@
 (async function() {
     const browserAPI = globalThis.browser ?? globalThis.chrome;
-    const ExtensionKernel = await import(browserAPI.runtime.getURL('js/ExtensionKernel.js'));
+    const ExtensionKernel = await import(browserAPI.runtime.getURL('src/js/ExtensionKernel.js'));
     ExtensionKernel.default.boot();
 })();
