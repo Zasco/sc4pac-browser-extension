c8462a1b3d778bedb3b5765a0666cd5805632359
Zasco <zasco_0@protonmail.com>
2025-01-31 19:26:48 -0500

    Added script paths config

---
+++ scripts/config/paths.js
@@ -0,0 +1,9 @@
+/**
+ * Defines common paths used across build and utility scripts.
+ * @property {string} NODE - Path to "node_modules" directory.
+ * @property {string} VENDOR - Path to "vendor" directory containing dependency libraries.
+ */
+export default {
+    NODE: 'node_modules/',
+    VENDOR: 'vendor/',
+}
